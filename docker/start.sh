42eeadad6eb4a91ba27294bacbc2d5ff5a0c9f89
Abdul Hayy Khan <ahkofficial.10@gmail.com>
2026-05-26 12:49:06 +0500

    deployment fixes

--- docker/start.sh
+++ docker/start.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 set -e
 
-# Ensure storage & cache dirs exist and have permissions
+cd /var/www/html
+
+# Fix storage permissions
 mkdir -p storage/framework/{sessions,views,cache} bootstrap/cache
 chown -R www-data:www-data storage bootstrap/cache || true
+
+# Run migrations automatically on startup
+php artisan migrate --force || true
 
 # Start php-fpm in background
 php-fpm &
 
 # Start nginx in foreground
-nginx -g 'daemon off;'
+exec nginx -g 'daemon off;'
